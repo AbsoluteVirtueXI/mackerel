ef91012c75586e01ffa51c011c257c40c7612fcd
AbsoluteVirtueXI <absolutevirtuexi@gmail.com>
2022-08-31 02:08:05 +0200

    placeholder

--- mackerel/src/print.rs
+++ mackerel/src/print.rs
@@ -1,26 +1,30 @@
+// TODO add ansi_term/colored/termcolor colors to the print macro's
+// maybe should also be used by log module.
+pub enum Color {}
+
 #[macro_export]
-macro_rules! print_xpr {
+macro_rules! p {
     ($a:expr) => {
         println!("{}", $a);
     };
 }
 
 #[macro_export]
-macro_rules! eprint_xpr {
+macro_rules! ep {
     ($a:expr) => {
         eprintln!("{}", $a)
     };
 }
 
 #[macro_export]
-macro_rules! debug_print_xpr {
+macro_rules! dp {
     ($a:expr) => {
         println!("{:?}", $a);
     };
 }
 
 #[macro_export]
-macro_rules! debug_eprint_xpr {
+macro_rules! dep {
     ($a:expr) => {
         eprintln!("{:?}", $a)
     };
