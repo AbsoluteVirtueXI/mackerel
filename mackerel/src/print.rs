9e0929470661323c56154072a285efb0e8aa9933
AbsoluteVirtueXI <absolutevirtuexi@gmail.com>
2022-09-06 15:18:14 +0200

    a better p! macro

--- mackerel/src/print.rs
+++ mackerel/src/print.rs
@@ -1,31 +1,51 @@
 // TODO add ansi_term/colored/termcolor colors to the print macro's
+// TODO p! should accept a list of expression
+// Insert debug printing as a "d:" or "debug:" tag
+//
 // maybe should also be used by log module.
 pub enum Color {}
 
 #[macro_export]
 macro_rules! p {
-    ($a:expr) => {
-        println!("{}", $a);
+    () => {
+        println!();
     };
+    (?$($xpr:expr),+ $(,)?) => {
+        $( println!("{:?}", $xpr); )*
+    };
+    (=$($xpr:expr),+ $(,)?) => {
+        $( println!("{} = {}", stringify!($xpr), $xpr); )*
+    };
+    // TODO: find a way to handle "?="" and "=?" in a one pattern
+    (=?$($xpr:expr),+ $(,)?) => {
+        $( println!("{} = {:?}", stringify!($xpr), $xpr); )*
+    };
+    (?=$($xpr:expr),+ $(,)?) => {
+        $( println!("{} = {:?}", stringify!($xpr), $xpr); )*
+    };
+    ($($xpr:expr),+ $(,)?) => {
+        $( println!("{}", $xpr); )*
+    }
 }
 
 #[macro_export]
 macro_rules! ep {
-    ($a:expr) => {
-        eprintln!("{}", $a)
+    () => {
+        eprintln!();
     };
-}
-
-#[macro_export]
-macro_rules! dp {
-    ($a:expr) => {
-        println!("{:?}", $a);
+    (?$($xpr:expr),+ $(,)?) => {
+        $( eprintln!("{:?}", $xpr); )*
     };
-}
-
-#[macro_export]
-macro_rules! dep {
-    ($a:expr) => {
-        eprintln!("{:?}", $a)
+    (=$($xpr:expr),+ $(,)?) => {
+        $( eprintln!("{} = {}", stringify!($xpr), $xpr); )*
     };
+    (=?$($xpr:expr),+ $(,)?) => {
+        $( eprintln!("{} = {:?}", stringify!($xpr), $xpr); )*
+    };
+    (?=$($xpr:expr),+ $(,)?) => {
+        $( eprintln!("{} = {:?}", stringify!($xpr), $xpr); )*
+    };
+    ($($xpr:expr),+ $(,)?) => {
+        $( eprintln!("{}", $xpr); )*
+    }
 }
