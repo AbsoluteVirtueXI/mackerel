ec907302ecd9c0790496862a74bb889ee2a9a171
AbsoluteVirtueXI <absolutevirtuexi@gmail.com>
2022-09-06 15:18:50 +0200

    get default value of type's instance

--- mackerel/src/types.rs
+++ mackerel/src/types.rs
@@ -1,2 +1,15 @@
 // TODO implement macro for default of a value;
 // TODO can also try to call associated functions directly from instances?
+fn default_of_helper<T>(_: &T) -> T
+where
+    T: Default,
+{
+    T::default()
+}
+
+#[macro_export]
+macro_rules! default_of {
+    ($a:expr) => {
+        default_of_helper(&$a)
+    };
+}
